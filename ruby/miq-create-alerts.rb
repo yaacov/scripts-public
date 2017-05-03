99bc8a6568af77a87ccdc8704abc4822fc27b3ae
yaacov <yzamir@redhat.com>
2017-05-03 14:51:00 +0300

    Add sevirity

--- ruby/miq-create-alerts.rb
+++ ruby/miq-create-alerts.rb
@@ -1,26 +1,59 @@
 #!/usr/bin/env ruby
+# Load Rails
+ENV['RAILS_ENV'] = ARGV[0] || 'development'
+
 require File.join(Dir.pwd, 'config/environment')
 
 ALERT_UUID="17ac4258-cd1e-11e6-914b-024234304b62"
-MiqAlert.where(:guid => ALERT_UUID).destroy_all # will destroy statuses and action(?) too
-ma = MiqAlert.create!(guid: ALERT_UUID, description: "All hawkular", options: {:notifications=>{:delay_next_evaluation=>600, :evm_event=>{}, :automate=>{:event_name=>"Oh no, an alert"}}}, db: "ContainerNode", expression: {:eval_method=>"hwk_generic", :mode=>"internal", :options=>{}})
-# create new MAS based on the existing ones
 
+MiqAlert.where(:guid => ALERT_UUID).destroy_all # will destroy statuses and action(?) too
+ma = MiqAlert.create!(
+  :guid        => ALERT_UUID,
+  :description => "All hawkular",
+  :options     => { 
+    :notifications => {
+      :delay_next_evaluation=> 600,
+      :evm_event            => {},
+      :automate             => {
+        :event_name => "Oh no, an alert"
+      }
+    }
+  },
+  :db          => "ContainerNode",
+  :expression  => {
+    :eval_method => "hwk_generic",
+    :mode        => "internal",
+    :options     => {}
+  }
+)
+
+# create new MAS based on the existing ones
 ExtManagementSystem.where(:type => "ManageIQ::Providers::Openshift::ContainerManager").each do |ext|
   mas = MiqAlertStatus.create!(
-    :miq_alert_id          => ma.id,
-    :resource_id           => ext.container_nodes.first.id,
-    :resource_type         => 'ContainerNode',
-    :evaluated_on          => Time.zone.now,
-    :ems_id => ext.id
+    :miq_alert_id  => ma.id,
+    :resource_id   => ext.container_nodes.first.id,
+    :resource_type => 'ContainerNode',
+    :evaluated_on  => Time.zone.now,
+    :severity      => "error",
+    :ems_id        => ext.id
   )
 
   mas2 = MiqAlertStatus.create!(
-      :miq_alert_id  => ma.id,
-      :resource_id   => ext.container_nodes.second.id,
-      :resource_type => 'ContainerNode',
-      :evaluated_on  => Time.zone.now,
-      :ems_id => ext.id
+    :miq_alert_id  => ma.id,
+    :resource_id   => ext.container_nodes.second.id,
+    :resource_type => 'ContainerNode',
+    :evaluated_on  => Time.zone.now,
+    :severity      => "warning",
+    :ems_id        => ext.id
+  )
+
+  mas3 = MiqAlertStatus.create!(
+    :miq_alert_id  => ma.id,
+    :resource_id   => ext.container_nodes.second.id,
+    :resource_type => 'ContainerNode',
+    :evaluated_on  => Time.zone.now,
+    :severity      => "warning",
+    :ems_id        => ext.id
   )
 
   MiqAlertStatusAction.create!(
